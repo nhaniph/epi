64263f7d680219f5b3c47bf29ab6cf29fd840089
Noah Haniph <nhaniph0621@gmail.com>
2024-12-12 17:52:08 -0500

    parity solution

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,8 +2,21 @@ from test_framework import generic_test
 
 
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    """
+    The parity of a binary word is 1 if the number of 1's in the word is odd;
+    Otherwise, it is 0. 
+    
+    For example, the parity of 1011 is 1 and the parity of 10001000 is 0.
+    
+    How would you compute the parity of a very large number of 64-bit words?
+    """
+    bit_count = 0
+    while (x):
+        bit_count += x & 1
+        
+        x >>= 1
+    
+    return bit_count % 2 != 0
 
 
 if __name__ == '__main__':
